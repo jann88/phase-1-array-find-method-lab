91d7993459674301dadb9af588a7622eac1b5a1a
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-06-17 14:13:39 +0300

    commit tree 7ba24b1fe6d8e5f356df5e31c41a14ca38b4fbbb

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record) {
+    let result = record.find(obj => obj.result === 'W');
+
+    if (result) {
+        return result.year;
+    } else {
+        return undefined;
+    }
+}
